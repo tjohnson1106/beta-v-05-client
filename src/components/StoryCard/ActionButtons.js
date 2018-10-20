deca97c2d26a7c38c25f06e14b8598456b9d1fd0
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-10-20 17:49:36 -0400

    created initial icon layout for photo card

--- src/components/StoryCard/ActionButtons.js
+++ src/components/StoryCard/ActionButtons.js
@@ -2,18 +2,28 @@ import React, { Component } from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
 import AntDesign from "react-native-vector-icons/AntDesign";
 
+import { makeHitSlop } from "../../utils/themes";
+
 class ActionButtons extends Component {
   state = {};
   render() {
     return (
       <View style={styles.root}>
         <View style={styles.actionWrapper}>
-          <TouchableOpacity style={styles.actionBtn}>
-            <AntDesign name="hearto" size={30} />
+          <TouchableOpacity style={styles.actionBtn} hitSlop={makeHitSlop(20)}>
+            <AntDesign name="hearto" size={27} color="#00001F" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.actionBtn} hitSlop={makeHitSlop(20)}>
+            <AntDesign name="message1" size={27} color="#00001F" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.actionBtn} hitSlop={makeHitSlop(20)}>
+            <AntDesign name="retweet" size={27} color="#00001F" />
           </TouchableOpacity>
         </View>
-        <View>{}</View>
-        <View>{}</View>
+        <View style={styles.fakeView} />
+        <TouchableOpacity style={styles.bookmarkWrapper} hitSlop={makeHitSlop(20)}>
+          <AntDesign name="book" size={27} color="#00001F" />
+        </TouchableOpacity>
       </View>
     );
   }
@@ -22,15 +32,27 @@ class ActionButtons extends Component {
 const styles = StyleSheet.create({
   root: {
     height: 50,
-    backgroundColor: "red",
-    paddingHorizontal: 16
+    paddingHorizontal: 16,
+    flexDirection: "row"
   },
   actionWrapper: {
     flex: 1,
-    backgroundColor: "dodgerblue"
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center"
   },
   actionBtn: {
-    flex: 1
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  fakeView: {
+    flex: 1.6
+  },
+  bookmarkWrapper: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "flex-end"
   }
 });
 
