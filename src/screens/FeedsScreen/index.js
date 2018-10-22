68ffddce2dc4a66fa577dcc2e6d3a69a749105cc
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-10-21 20:10:38 -0400

    created refresh control properties

--- src/screens/FeedsScreen/index.js
+++ src/screens/FeedsScreen/index.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
-import { FlatList, ActivityIndicator, StyleSheet, View } from "react-native";
+import {
+  FlatList,
+  ActivityIndicator,
+  StyleSheet,
+  View,
+  RefreshControl
+} from "react-native";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 
 import { StoryCard } from "../../components";
 
 class FeedsScreen extends Component {
-  state = {};
+  state = {
+    isRefreshing: false
+  };
 
   _keyExtractor = item => item.id;
 
   _renderItem = ({ item }) => <StoryCard data={item} />;
+
+  _refreshRequest = async () => {
+    this.setState({
+      isRefreshing: true
+    });
+    await this.props.data.refetch();
+    this.setState({
+      isRefreshing: false
+    });
+  };
 
   render() {
     if (this.props.data.loading) {
@@ -26,6 +44,12 @@ class FeedsScreen extends Component {
         data={this.props.data.photos}
         keyExtractor={this._keyExtractor}
         renderItem={this._renderItem}
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.isRefreshing}
+            onRefresh={this._refreshRequest}
+          />
+        }
       />
     );
   }
