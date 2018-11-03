df1d7913f3385296159e053b329ba577e1a54a3c
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-11-03 17:47:37 -0400

    created login screen layout

--- src/screens/LoginScreen/index.js
+++ src/screens/LoginScreen/index.js
@@ -1,23 +1,46 @@
+// @flow
+
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { iOSColors } from "react-native-typography";
 
 class LoginScreen extends Component {
-  state = {};
   render() {
     return (
       <View style={styles.root}>
-        <Text>Login Screen</Text>
+        <View style={styles.header}>
+          <Text>Vertigo</Text>
+        </View>
+        <View style={styles.content}>
+          <Text>Content</Text>
+        </View>
       </View>
     );
   }
 }
-
-export default LoginScreen;
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  header: {
+    flex: 0.3,
+    backgroundColor: blue,
+    alignSelf: "stretch",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  appName: {
+    color: iOSColors.white,
+    fontSize: 50
+  },
+  content: {
+    flex: 1,
+    backgroundColor: "red",
+    alignSelf: "stretch"
   }
 });
+
+export default LoginScreen;
